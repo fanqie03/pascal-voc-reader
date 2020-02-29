c1f8682b3d45deabd2c097b29ad0408d4f956d47
mengfu188 <1242733702@qq.com>
2020-02-29 17:42:28 +0800

    Add show func

--- pascal_voc_reader/__init__.py
+++ pascal_voc_reader/__init__.py
@@ -18,6 +18,7 @@ class Reader:
         self.txt_root = self.voc_root / 'ImageSets' / 'Main'
         self.img_root = self.voc_root / 'JPEGImages'
         self.df = pd.DataFrame(self.parse_xml(self.xml_root))
+        self.df['img_path'] = self.df.stem.map(lambda x: self.img_root / (x + '.jpg'))
         train_array, val_array, train_val_array, test_array = self.parse_txt(self.txt_root)
         self.df['train'] = self.df.stem.isin(train_array)
         self.df['val'] = self.df.stem.isin(val_array)
@@ -57,7 +58,7 @@ class Reader:
             ratio = h / w
             ret.append(dict(name=name, difficult=difficult,
                 x1=bbox[0], y1=bbox[1], x2=bbox[2], y2=bbox[3],
-                filename=xml_path.name, stem=xml_path.stem,
+                xml_path=xml_path, stem=xml_path.stem,
                 width=width, height=height, depth=depth,
                 h=h, w=w, ratio=ratio))
         return ret
@@ -74,6 +75,31 @@ class Reader:
             return []
         with open(txt_path) as f:
             return list(f.readlines())
+
+    def show(self):
+        import cv2
+        grouped = self.df.groupby('stem')
+        # length = self.df.
+        length = self.df.stem.unique().size
+        indexes = self.df.stem.unique()
+        def nothing(x):
+            index = indexes[x]
+            sub_table = grouped.get_group(index)
+            img_path = str(sub_table.iloc[0]['img_path'])
+            img = cv2.imread(img_path)
+            for index, row in sub_table.iterrows():
+                cv2.rectangle(img, (row['x1'], row['y1']), (row['x2'], row['y2']),
+                              (0, 255, 0))
+            # img = cv2.resize(img, (800, 800))
+            cv2.imshow('image_win', img)
+            # cv2.waitKey()
+
+        cv2.namedWindow('image_win')
+        cv2.namedWindow('tracker_win')
+        cv2.createTrackbar('tracker', 'tracker_win', 0, length - 1, nothing)
+
+        if cv2.waitKey() == 'q':
+            cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
